8a72162588beb8cb3657fb8aa8b6593870e6ce91
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2020-06-11 11:17:21 -0500

    [+] Component > MainLayout: Add global styles to the project using emotion

--- src/components/ui/MainLayout.js
+++ src/components/ui/MainLayout.js
@@ -2,10 +2,57 @@ import React from 'react';
 
 /** Dependencies */
 import { Helmet } from 'react-helmet';
+import { Global, css } from '@emotion/core';
 
+/** Component */
 const MainLayout = ({ children }) => {
     return(
         <>
+            <Global styles={ 
+                css `
+                    html {
+                        box-sizing: border-box;
+                        font-size: 62.5%;
+                    }
+
+                    *, *:after, *:before {
+                        box-sizing: inherit;
+                    }
+
+                    body {
+                        font-family: 'Lato', sans-serif;
+                        font-size: 1.6rem;
+                        line-height: 2;
+                    }
+
+                    h1, h2, h3 {
+                        line-height: 1.5;
+                        margin: 0;
+                    }
+
+                    h2, h3 {
+                        font-family: 'Lato', sans-serif;
+                        text-align: center;
+                        font-weight: 300;
+                    }
+
+                    h3 {
+                        font-family: 'Roboto', sans-serif;
+                    }
+
+                    ul {
+                        margin: 0;
+                        list-style: none:
+                        padding: 0;
+                    }
+
+                    .container {
+                        margin: 0 auto;
+                        max-width 120rem:
+                        width: 95%;
+                    }
+                `}
+            />
             <Helmet>
                 <meta charSet="utf-8" />
                 <meta name="description" content="Sitio web de bienes raices en Gatsby" />
