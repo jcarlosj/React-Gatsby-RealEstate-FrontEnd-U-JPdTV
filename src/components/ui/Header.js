4d5440ef219210d6da9fcd88eac2217aec2261af
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2020-06-11 15:52:00 -0500

    [+] Component > Header: GraphQL query that gets logo URL

--- src/components/ui/Header.js
+++ src/components/ui/Header.js
@@ -1,14 +1,47 @@
 import React from 'react';
-import { Link } from 'gatsby';
+import { Link, useStaticQuery, graphql } from 'gatsby';
 
 /** Dependencies */
 import { css } from '@emotion/core';
+import styled from '@emotion/styled';
 
 /** Components */
 import Navigation from './Navigation';
 
+/** Style Components */
+const Logo = styled( Link ) `
+    color: white;
+    display: flex;
+    text-decoration: none;
+
+    h2 {
+        padding-left: 1rem;
+
+        span {
+            font-weight: 700;
+            text-transform: uppercase;
+        }
+    }
+
+    img {
+        width: 4rem;
+    }
+`;
+
 /** Component */
 const Header = () => {
+
+    /** Query GraphQL para obtener la URL del Logo */
+    const { logo } = useStaticQuery( graphql `
+        query {
+            logo: file( relativePath: { eq: "logo.svg" } ), {
+                publicURL
+            }
+        }
+    `);
+
+    console .log( 'Logo', logo );
+
     return(
         <header css={ css `
             background-color: #0D283B;
@@ -25,7 +58,13 @@ const Header = () => {
                     justify-content: space-between;
                 }
             `}>
-                <Link to="/">Bienes raices</Link>
+                <Logo to="/" >
+                    <img 
+                        src={ logo .publicURL } 
+                        alt="Gatsby Inmobiliaria" 
+                    />
+                    <h2>Gatsby <span>Inmobiliaria</span></h2>
+                </Logo>
                 <Navigation />
             </div>
         </header>
