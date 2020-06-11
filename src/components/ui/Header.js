a367f96eccbb28c5ae25755215df8101b29837ef
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2020-06-11 14:37:50 -0500

    [+] Components > Header, Navigation: Stylesheet

--- src/components/ui/Header.js
+++ src/components/ui/Header.js
@@ -1,17 +1,33 @@
 import React from 'react';
 import { Link } from 'gatsby';
 
+/** Dependencies */
+import { css } from '@emotion/core';
+
 /** Components */
 import Navigation from './Navigation';
 
 /** Component */
 const Header = () => {
     return(
-        <header>
-            <div>
+        <header css={ css `
+            background-color: #0D283B;
+            padding: 1rem;
+        `}>
+            <div css={ css `
+                margin: 0 auto;
+                max-width: 120rem;
+                text-align: center;
+
+                @media ( min-width: 768px ) {
+                    align-items: center;
+                    display: flex;
+                    justify-content: space-between;
+                }
+            `}>
                 <Link to="/">Bienes raices</Link>
+                <Navigation />
             </div>
-            <Navigation />
         </header>
     );
 }
--- src/components/ui/Navigation.js
+++ src/components/ui/Navigation.js
@@ -1,12 +1,51 @@
 import React from 'react';
 import { Link } from 'gatsby';
 
+/** Dependencies */
+import styled from '@emotion/styled';
+
+/** Style Components */ 
+const 
+    Nav = styled.nav `
+        display: flex;
+        flex-direction: column;
+        padding-bottom: 3rem;
+
+        @media ( min-width: 768px ) {
+            flex-direction: row;
+            padding: 0;
+        }
+    `,
+    NavLink = styled( Link ) `
+        color: white;
+        font-family: 'PT Sans', sans-serif;
+        font-weight: 700;
+        margin-right: 1rem; 
+        padding: .5rem;
+        text-decoration: none;
+
+        &:last-of-type {
+            margin-right: 0;
+        }
+
+        &.current-page {
+            border-bottom: 2px solid white;
+        }
+    `;
+
+/** Component */
 const Navigation = () => {
     return(
-        <nav>
-            <Link to="/us">Nosotros</Link>
-            <Link to="/real-estate">Propiedades</Link>
-        </nav>
+        <Nav>
+            <NavLink 
+                to="/us"
+                activeClassName="current-page"
+            >Nosotros</NavLink>
+            <NavLink 
+                to="/real-estate"
+                activeClassName="current-page"
+            >Propiedades</NavLink>
+        </Nav>
     );
 }
 
