8be62cb29d45fb7478d14cb7bfb894329a7adfd7
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2020-06-12 18:27:56 -0500

    [+] Component > Banner: Add gatsby fragment to improve image upload performance & Display image of page component

--- src/components/ui/Banner.js
+++ src/components/ui/Banner.js
@@ -1,6 +1,18 @@
 import React from 'react';
 import { graphql, useStaticQuery } from 'gatsby';
 
+/** Style Modules */
+import indexStyle from '../../css/index.module.css';
+
+/** Dependencies */
+import styled from '@emotion/styled';
+import BackgroundImage from 'gatsby-background-image';
+
+/** Style Components */
+const Background = styled( BackgroundImage ) `
+    height: 60rem;
+`;
+
 /** Component */
 const Banner = () => {
 
@@ -8,17 +20,30 @@ const Banner = () => {
     const { image } = useStaticQuery( graphql `
         query {
             image: file ( relativePath: { eq: "encuentra.jpg" } ) {
-                publicURL
+                sharp: childImageSharp {
+                    fluid( maxWidth: 1200 ) {
+                        ...GatsbyImageSharpFluid_withWebp
+                    }
+                }
             }
         }
     `);
 
     console .log( 'Banner', image );
 
+    const { sharp: { fluid } } = image;
+
     return( 
-        <>
-            <h2>Banner</h2>
-        </> 
+        <Background
+            tag="section"
+            fluid={ fluid }
+            fadeIn="soft"
+        >
+            <div className={ indexStyle .bgImg }>
+                <h2 className={ indexStyle .title }>Encuentra el hogar de tus sueños</h2>
+                <p>Con 15 años de experiencia en el sector</p>
+            </div>
+        </Background>
     );
 }
 
