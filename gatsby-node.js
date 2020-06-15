686720e0f89a5e23c27338edc950fc55932c3859
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2020-06-15 17:55:16 -0500

    [+] File > gatsby-node.js (dynamic page): GraphQL query to get id and name real-estate

--- gatsby-node.js
+++ gatsby-node.js
@@ -5,3 +5,27 @@
  */
 
 // You can delete this file if you're not using it
+
+/** Crea páginas dinámicas */
+exports .createPages = async ({ actions, graphql, reporter }) => {
+
+    /** GraphQL Query */
+    const response = await graphql( `
+        query {
+            allStrapiRealEstate {
+                nodes {
+                    id
+                    name
+                }
+            }
+        }
+    `);
+
+    /** Valida si no se obtuvo resultados */
+    if( response .errors ) {
+        reporter .panic( 'No se obtuvo resultados', response .errors );
+    }
+
+    console .log( 'createPages', JSON .stringify( response ) );
+
+}
