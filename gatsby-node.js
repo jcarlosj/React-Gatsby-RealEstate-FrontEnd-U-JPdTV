b207dadf8f4820ce78fec1b1333bcd078db80bde
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2020-06-15 18:45:43 -0500

    [+] NPM > url-slug: Install and implement the slug generator

--- gatsby-node.js
+++ gatsby-node.js
@@ -6,6 +6,9 @@
 
 // You can delete this file if you're not using it
 
+/** Dependencies */
+const urlSlug = require( 'url-slug' );
+
 /** Crea páginas dinámicas */
 exports .createPages = async ({ actions, graphql, reporter }) => {
 
@@ -26,6 +29,17 @@ exports .createPages = async ({ actions, graphql, reporter }) => {
         reporter .panic( 'No se obtuvo resultados', response .errors );
     }
 
-    console .log( 'createPages', JSON .stringify( response ) );
+    const registers = response .data .allStrapiRealEstate .nodes;
+
+    console .log( 'createPages', registers );
+
+    /** Recorre cada registro obtenido */
+    registers .forEach( realEstate => {
+        const slug = urlSlug( realEstate .name );       // Convierte un string en un slug
+
+        console .group( 'name', realEstate .name );
+        console .log( 'slug', slug );    
+        console .groupEnd();
+    });
 
 }
