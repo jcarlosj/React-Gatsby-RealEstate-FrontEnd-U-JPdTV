100814e1bc0e3e31c6d3493f5beb62cfe75e0086
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2020-06-18 14:05:11 -0500

    [+] Component (dPage) > Property: Display information about the real estate

--- src/components/page/Property.js
+++ src/components/page/Property.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import { graphql } from 'gatsby';
+import Image from 'gatsby-image';
 
 /** Components */
 import MainLayout from '../layout/MainLayout';
+import Icons from '../ui/Icons';
 
 /** GraphQL Query */
 export const query = graphql `
@@ -11,19 +13,62 @@ export const query = graphql `
             nodes {
                 id
                 name
+                description
+                price
+                rooms
+                bathrooms
+                parking_lot
+                category {
+                    name
+                }
+                agent {
+                    name
+                    phone
+                    email
+                }
+                picture {
+                    sharp: childImageSharp {
+                        fluid( maxWidth: 1200 ) {
+                            ...GatsbyImageSharpFluid_withWebp
+                        }
+                    }
+                }
             }
         }
     }
 `;
 
 /** Dynamic Page Component */
-const Property = ({ data }) => {
-
-    console .log( 'Property', data );
+const Property = ({ data: {  allStrapiRealEstate: { nodes } } }) => {
+    const 
+        { name, description, price, rooms, bathrooms, parking_lot, category, agent, picture } = nodes[ 0 ];
 
     return( 
         <MainLayout>
-            <h1>Propiedad</h1>
+            <h1>{ name }</h1>
+            <div>
+                <main>
+                    <Image 
+                        fluid={ picture .sharp .fluid }
+                        alt={ name }
+                    />
+                    <p>{ description }</p>
+                </main>
+                <aside>
+                    <p className="price">$ { price }</p>
+                    <Icons 
+                        rooms={ rooms }
+                        bathrooms={ bathrooms }
+                        parking_lot={ parking_lot }
+                    />
+                    <div>
+                        <h2>Agente comercial</h2>
+                        <p>{ agent .name }</p>
+                        <p>Teléfono: { agent .phone } </p>
+                        <p>Email: { agent .email } </p>
+                    </div>
+                </aside>
+            </div>
         </MainLayout>
     );
 }
